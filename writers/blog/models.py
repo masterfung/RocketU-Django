c29cb2edab9f58ea991f8d50672ee1a3f43d5016
Tsung Hung <hungmasterj@gmail.com>
2014-06-27 14:34:43 -0700

    update to the model example

--- writers/blog/models.py
+++ writers/blog/models.py
@@ -3,17 +3,35 @@ from django.db import models
 # Create your models here.
 
 class Author(models.Model):
-    name = models.CharField(max_length=120)
-    twitter = models.CharField(max_length=40, null=True)
+	name = models.CharField(max_length=120)
+	twitter = models.CharField(max_length=40, null=True)
 
-    def __unicode__(self):
-        return u"{}".format(self.name)
+	def __unicode__(self):
+		return u"{}".format(self.name)
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=120)
-    body = models.TextField()
-    author = models.ForeignKey(Author)
+	title = models.CharField(max_length=120)
+	body = models.TextField()
+	author = models.ForeignKey(Author, related_name='posts')
 
-    def __unicode__(self):
-        return u"{}".format(self.title)
+	def __unicode__(self):
+		return u"{}".format(self.title)
+
+
+class Tag(models.Model):
+	name = models.CharField(max_length=20)
+	posts = models.ManyToManyField(Post)
+
+	def __unicode__(self):
+		return u"{}".format(self.name)
+
+
+class User(models.Model):
+	pass
+
+class Comment(models.Model):
+	pass
+
+class Comment(models.Model):
+	pass

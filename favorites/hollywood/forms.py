1bf2007bbfc25e422e90fe8169f61db75fcd010f
Tsung Hung <hungmasterj@gmail.com>
2014-07-06 20:46:20 -0700

    more forms update

--- favorites/hollywood/forms.py
+++ favorites/hollywood/forms.py
@@ -1,17 +1,24 @@
 from django.forms import ModelForm
-from hollywood.models import Genre, Movie, Actor
+from hollywood.models import Genre, Movie, Actor, Video
 
 __author__ = '@masterfung'
 
 
 class GenreForm(ModelForm):
-    class Meta:
-        model = Genre
+	class Meta:
+		model = Genre
+
 
 class MovieForm(ModelForm):
-    class Meta:
-        model = Movie
+	class Meta:
+		model = Movie
+
 
 class ActorForm(ModelForm):
-    class Meta:
-        model = Actor
+	class Meta:
+		model = Actor
+
+
+class VideoForm(ModelForm):
+	class Meta:
+		model = Video

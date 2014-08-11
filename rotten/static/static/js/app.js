d0e0911afe619c9cdfb0d1477842416206ab41ad
Tsung Hung <hungmasterj@gmail.com>
2014-08-11 12:19:30 -0700

    rotten tomatoes growing

--- rotten/static/static/js/app.js
+++ rotten/static/static/js/app.js
@@ -7,7 +7,7 @@ rotten.factory('MovieApi', function () {
 	return '88a8qpv9kwg657jxb97ma5nn';
 });
 
-rotten.controller('rottenCtrl', function ($scope, $http, MovieApi) {
+rotten.controller('upcomingCtrl', function ($scope, $http, MovieApi) {
 	$scope.rottenList = [];
 	$scope.rottenImage = [];
 	$http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters', {
@@ -17,16 +17,38 @@ rotten.controller('rottenCtrl', function ($scope, $http, MovieApi) {
 		     callback: 'JSON_CALLBACK'
 		 }
 	}).then(function (promise) {
-		console.log(promise);
 		for (var i = 0; i < promise.data.movies.length; i++) {
 	     $scope.rottenList.push(promise.data.movies[i]);
-		 console.log(promise.data.movies[i].posters.profile);
 		 var image = promise.data.movies[i].posters.profile;
 		 var changeImage = image.replace('_tmb', '_det');
-		 console.log(changeImage);
 		 $scope.rottenImage.push(changeImage);
 	 }
-		console.log($scope.rottenList[1]);
-		console.log($scope.rottenImage);
 	})
+});
+
+rotten.controller('movieCtrl', function ($scope, $http) {
+	$scope.titles = [];
+	$scope.retrieveMovies = function () {
+		$http.jsonp(
+		 'http://api.rottentomatoes.com/api/public/v1.0/movies', {
+				params: {
+					apikey: '88a8qpv9kwg657jxb97ma5nn',
+					q: $scope.movies,
+					page_limit: '10',
+					callback: 'JSON_CALLBACK'
+				}
+			}).then(function (promise) {
+				console.log(promise.data.movies);
+				for (var i = 0; i < promise.data.movies.length; i++) {
+					$scope.titles.push(promise.data.movies[i]);
+				}
+				$scope.showResults = true;
+
+			}
+		);
+	};
+	$scope.showMovieInfo = function() {
+		$scope.movieDescription = $scope.movieDescription ===  false ? true: false;
+	};
+
 });
